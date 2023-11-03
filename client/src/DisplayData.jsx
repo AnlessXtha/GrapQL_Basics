abbac26e18867c039306ac9d603077163da5be1b
AnlessXtha <aadesh.shrrstha@gmail.com>
2023-11-03 09:59:10 +0545

    added create user feature in REACT using queries from graphql

--- client/src/DisplayData.jsx
+++ client/src/DisplayData.jsx
@@ -48,7 +48,7 @@ function DisplayData() {
   const [age, setAge] = useState("");
   const [nationality, setNationality] = useState("");
 
-  const { data, loading, error } = useQuery(QUERY_ALL_USERS);
+  const { data, loading, refetch } = useQuery(QUERY_ALL_USERS);
   const { data: movieData } = useQuery(QUERY_ALL_MOVIES);
   const [fetchMovie, { data: movieSearchData, error: movieSearchError }] =
     useLazyQuery(GET_MOVIE_BY_NAME);
@@ -91,14 +91,18 @@ function DisplayData() {
           type="text"
           placeholder="Nationality..."
           onChange={(event) => {
-            setNationality(event.target.value.toUpperCase);
+            setNationality(event.target.value.toUpperCase());
           }}
         />
         <button
           onClick={() => {
             createUser({
-              variables: { input: { name, username, age, nationality } },
+              variables: {
+                input: { name, username, age: Number(age), nationality },
+              },
             });
+
+            refetch();
           }}
         >
           Create User
@@ -109,7 +113,7 @@ function DisplayData() {
           return (
             <div>
               <h1>Name: {user.name}</h1>
-              <h1>Username: {user.userName}</h1>
+              <h1>Username: {user.username}</h1>
               <h1>Age: {user.age}</h1>
               <h1>Nationality: {user.nationality}</h1>
               <hr />
--- server/schema/type-defs.js
+++ server/schema/type-defs.js
@@ -53,6 +53,7 @@ const typeDefs = gql`
     INDIA
     GERMANY
     CHILE
+    NEPAL
   }
 `;
 
